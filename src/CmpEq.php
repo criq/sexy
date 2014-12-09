aecbb431b9533e91132014d3ee4b343516051761
Jan Jablunka <jablunka@gmail.com>
2014-12-09 12:59:25 +0100

    merge

---
+++ src/CmpEq.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sexy;
+
+class CmpEq extends Cmp {
+
+	public function getSql(&$context = []) {
+		return " ( " . $this->name->getSql($context) . (!is_null($this->value) ? " = " . $this->value->getSql($context) : null) . " ) ";
+	}
+
+}
