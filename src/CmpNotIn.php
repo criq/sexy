515d1f1a2604fafffcd036f923161bcd4759375e
Jan Jablunka <jablunka@gmail.com>
2015-01-13 15:20:50 +0100

    merge

---
+++ src/CmpNotIn.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sexy;
+
+class CmpNotIn extends Cmp {
+
+	public function getSql(&$context = []) {
+		return " ( " . $this->name->getSql($context) . " NOT IN ( " . $this->value->getSql($context) . " ) ) ";
+	}
+
+}
