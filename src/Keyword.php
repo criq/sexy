d8928a9714052284587b989ed44daf3ab9488cf4
Jan Jablunka <jablunka@gmail.com>
2015-01-13 10:09:50 +0100

    merge

---
+++ src/Keyword.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sexy;
+
+class Keyword extends Expression {
+
+	public $keyword;
+
+	public function __construct($keyword = null) {
+		$this->keyword = (string) $keyword;
+	}
+
+	public function getSql(&$context = []) {
+		return " " . $this->keyword . " ";
+	}
+
+}
