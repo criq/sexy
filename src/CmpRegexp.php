4e65ac93a3eb1409b73a436023f2d7f037b75c54
Jan Jablunka <jablunka@gmail.com>
2015-02-05 10:40:56 +0100

    merge

---
+++ src/CmpRegexp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sexy;
+
+class CmpRegexp extends Cmp {
+
+	public function getSql(&$context = []) {
+		return " ( " . $this->name->getSql($context) . " REGEXP " . $this->value->getSql($context) . " ) ";
+	}
+
+}
