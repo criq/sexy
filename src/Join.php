5d05f18e3e3964fbc298398433e1a51bfa15efeb
Jan Jablunka <jablunka@gmail.com>
2015-02-20 14:26:28 +0100

    merge

---
+++ src/Join.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Sexy;
+
+class Join extends Expression {
+
+	public $direction;
+	public $join;
+	public $conditions;
+	public $alias;
+
+	public function __construct(Expression $join, Expression $conditions, Keyword $direction = null, Alias $alias = null) {
+		$this->direction  = $direction;
+		$this->join       = $join;
+		$this->conditions = $conditions;
+		$this->alias      = $alias;
+
+		if ($this->join instanceof Select) {
+			$this->join->setOptGetTotalRows(false);
+		}
+	}
+
+	public function getSql(&$context = []) {
+		return (!is_null($this->direction) ? $this->direction->getSql($context) : null) . " JOIN ( " . $this->join->getSql($context) . " ) " . (!is_null($this->alias) ? " AS " . $this->alias->getSql($context) : null) . " ON ( " . $this->conditions->getSql($context) . " ) ";
+	}
+
+}
