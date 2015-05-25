96a4d08bbca66dfe253b22e09327446bba21ee7a
Jan Jablunka <jablunka@gmail.com>
2015-05-25 15:27:31 +0200

    merge

---
+++ src/CmpContains.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sexy;
+
+class CmpContains extends CmpLike {
+
+	public function __construct(Expression $name, $value = null) {
+		return parent::__construct($name, '%' . $value . '%');
+	}
+
+}
