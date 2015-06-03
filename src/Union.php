8d07a2368d3c4b6040cfa65877281dea98ffe4ab
Jan Jablunka <jablunka@gmail.com>
2015-06-03 10:46:39 +0200

    merge

---
+++ src/Union.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Sexy;
+
+class Union extends Expression {
+
+	public $arguments;
+
+	public function __construct($arguments = []) {
+		$this->arguments = is_array($arguments) ? $arguments : [$arguments];
+	}
+
+	public function addArgument($argument) {
+		return $this->arguments[] = $argument;
+	}
+
+	public function getSql(&$context = []) {
+		$sqls = [];
+
+		foreach ($this->arguments as $argument) {
+			$sqls[] = " ( " . $argument->getSql($context) . " ) ";
+		}
+
+		return implode(" UNION ", $sqls);
+	}
+
+}
