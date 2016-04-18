2ca1c5c469b164472e7cc3b0459c1c380e1e8b1c
Jan Jablunka <jablunka@gmail.com>
2016-04-18 11:08:13 +0200

    merge

---
+++ src/LgcAnd.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sexy;
+
+class LgcAnd extends Expression {
+
+	public $expressions = [];
+
+	public function __construct(array $expressions) {
+		$this->expressions = $expressions;
+	}
+
+	public function getSql(&$context = []) {
+		return " ( " . implode(" AND ", array_map(function($i) use(&$context) {
+			return " ( " . $i->getSql($context) . " ) ";
+		}, $this->expressions)) . " ) ";
+	}
+
+}
