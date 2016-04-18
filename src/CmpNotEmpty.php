5f30578a0b4853c58d19795db8c5e3755332d021
Jan Jablunka <jablunka@gmail.com>
2016-04-18 11:17:54 +0200

    merge

---
+++ src/CmpNotEmpty.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sexy;
+
+class CmpNotEmpty extends Cmp {
+
+	public function getSql(&$context = []) {
+		return " NOT ( " . $this->name->getSql($context) . " = '' ) ";
+	}
+
+}
---
+++ src/Expression.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sexy;
+
+abstract class Expression {
+
+	abstract public function getSql(&$context = []);
+
+	public function getSqlWithValues() {
+		$context = [
+			'useBindValues' => false,
+		];
+
+		return $this->getSql($context);
+	}
+
+	public function __toString() {
+		return $this->getSqlWithValues();
+	}
+
+}
