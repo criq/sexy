381a7209879ab6fe71333e425f8add0ae1001d3b
Jan Jablunka <jablunka@gmail.com>
2017-11-22 12:34:20 +0100

    incremental

--- src/CmpBetween.php
+++ src/CmpBetween.php
@@ -10,12 +10,12 @@ class CmpBetween extends Cmp {
 
 	public function __construct($column, $minValue, $maxValue) {
 		$this->column  = $column;
-		if ($minValue instanceof BindValue) {
+		if ($minValue instanceof Expression) {
 			$this->minValue = $minValue;
 		} else {
 			$this->minValue = new BindValue($minValue);
 		}
-		if ($maxValue instanceof BindValue) {
+		if ($maxValue instanceof Expression) {
 			$this->maxValue = $maxValue;
 		} else {
 			$this->maxValue = new BindValue($maxValue);
