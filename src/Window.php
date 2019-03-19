28db497faa5222c8b40563962bcc5c58b5f4cab9
Jan Jablunka <jablunka@gmail.com>
2019-03-19 15:01:57 +0100

    incremental

---
+++ src/Window.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sexy;
+
+class Window extends Expression {
+
+	public $partition;
+	public $orderBy;
+
+	public function __construct($partition, $orderBy = null) {
+		$this->partition = $partition instanceof Expression ? [$partition] : $partition;
+		$this->orderBy = $orderBy instanceof Expression ? [$orderBy] : $orderBy;
+	}
+
+	public function getSql(&$context = []) {
+		$sql = null;
+
+		if ($this->partition) {
+			$partitionBySql = trim(implode(" , ", array_map(function($e) use($context) { return $e->getSql($context); }, $this->partition)));
+			if ($partitionBySql) {
+				$sql .= " PARTITION BY " . $partitionBySql;
+			}
+		}
+
+		if ($this->orderBy) {
+			$orderBySql = trim(implode(" , ", array_map(function($e) use($context) { return $e->getSql($context); }, $this->orderBy)));
+			if ($orderBySql) {
+				$sql .= " ORDER BY " . $orderBySql;
+			}
+		}
+
+		return $sql;
+	}
+
+}
