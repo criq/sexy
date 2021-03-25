1c5764f6832eeb63632778b19c81d892e7191769
Jan Jablunka <jablunka@gmail.com>
2021-03-25 17:38:48 +0100

    incremental

--- src/Page.php
+++ src/Page.php
@@ -4,11 +4,11 @@ namespace Sexy;
 
 class Page
 {
-	const DEFAULT_PAGE    = 1;
+	const DEFAULT_PAGE = 1;
 	const DEFAULT_PERPAGE = 50;
 
 	public $perPage = self::DEFAULT_PERPAGE;
-	public $page    = self::DEFAULT_PAGE;
+	public $page = self::DEFAULT_PAGE;
 
 	public function __construct($page = self::DEFAULT_PAGE, $perPage = self::DEFAULT_PERPAGE)
 	{
@@ -23,17 +23,27 @@ class Page
 		$this->page = (int)$page;
 	}
 
-	public static function createFromOffsetAndLimit($offset, $limit)
+	public static function createFromOffsetAndLimit($offset, $limit) : Page
 	{
 		return new static(floor($offset / $limit) + 1, $limit);
 	}
 
-	public function getOffset()
+	public function getPage() : int
+	{
+		return (int)$this->page;
+	}
+
+	public function getPerPage() : int
+	{
+		return (int)$this->perPage;
+	}
+
+	public function getOffset() : int
 	{
 		return (int)(($this->page * $this->perPage) - $this->perPage);
 	}
 
-	public function getLimit()
+	public function getLimit() : int
 	{
 		return (int)$this->perPage;
 	}
