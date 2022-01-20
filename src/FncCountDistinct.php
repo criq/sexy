aba5cc0d5ff51bab66ee496a495e2bc87a628b55
Jan Jablunka <jablunka@gmail.com>
2022-01-20 16:21:24 +0100

    incremental

--- src/FncCountDistinct.php
+++ src/FncCountDistinct.php
@@ -2,12 +2,12 @@
 
 namespace Sexy;
 
-class FncCountDistinct extends FnCount {
-
-	public function getSql(&$context = []) {
-		return " " . trim(strtoupper($this->function->getSql($context))) . "( DISTINCT " . implode(", ", array_map(function($i) use(&$context) {
+class FncCountDistinct extends FncCount
+{
+	public function getSql(&$context = [])
+	{
+		return " " . trim(strtoupper($this->function->getSql($context))) . "( DISTINCT " . implode(", ", array_map(function ($i) use (&$context) {
 			return $i->getSql($context);
 		}, $this->arguments)) . " ) " . (!is_null($this->alias) ? " AS " . $this->alias->getSql($context) : null);
 	}
-
 }
