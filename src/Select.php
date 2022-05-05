ec39ed2430df44e0df8d7fedc3a98c2b1aed9028
Jan Jablunka <jablunka@gmail.com>
2022-05-05 11:17:55 +0200

    incremental

--- src/Select.php
+++ src/Select.php
@@ -17,14 +17,14 @@ class Select extends Command
 
 	public function setGetFoundRows(bool $value = true): Select
 	{
-		$this->setFlag('getTotalRows', $value);
+		$this->setFlag("getTotalRows", $value);
 
 		return $this;
 	}
 
 	public function setGetDistinctRows(bool $value = true): Select
 	{
-		$this->setFlag('getDistinctRows', $value);
+		$this->setFlag("getDistinctRows", $value);
 
 		return $this;
 	}
@@ -36,13 +36,13 @@ class Select extends Command
 		return $this;
 	}
 
-	public function setIntoOutfile(\Katu\Files\File $file, $fieldsTerminatedBy = ",", $optionallyEnclosedBy = '"', $escapedBy = '\\\\', $linesTerminatedBy = '\n'): Select
+	public function setIntoOutfile(\Katu\Files\File $file, $fieldsTerminatedBy = ",", $optionallyEnclosedBy = "\"", $escapedBy = "\\\\", $linesTerminatedBy = "\n"): Select
 	{
-		$this->setFlag('intoOutfile', (string)$file);
-		$this->setFlag('fieldsTerminatedBy', (string)$fieldsTerminatedBy);
-		$this->setFlag('optionallyEnclosedBy', (string)$optionallyEnclosedBy);
-		$this->setFlag('escapedBy', (string)$escapedBy);
-		$this->setFlag('linesTerminatedBy', (string)$linesTerminatedBy);
+		$this->setFlag("intoOutfile", (string)$file);
+		$this->setFlag("fieldsTerminatedBy", (string)$fieldsTerminatedBy);
+		$this->setFlag("optionallyEnclosedBy", (string)$optionallyEnclosedBy);
+		$this->setFlag("escapedBy", (string)$escapedBy);
+		$this->setFlag("linesTerminatedBy", (string)$linesTerminatedBy);
 
 		return $this;
 	}
@@ -51,42 +51,42 @@ class Select extends Command
 	{
 		$sql = " SELECT ";
 
-		if ($this->getFlag('getDistinctRows')) {
+		if ($this->getFlag("getDistinctRows")) {
 			$sql .= " DISTINCT ";
 		}
 
-		if ($this->getFlag('getTotalRows')) {
+		if ($this->getFlag("getTotalRows")) {
 			$sql .= " SQL_CALC_FOUND_ROWS ";
 		}
 
-		if (count($this->getExpressions('select'))) {
-			$sql .= $this->getExpressions('select')->setDelimiter(", ")->getSql($context);
+		if (count($this->getExpressions("select"))) {
+			$sql .= $this->getExpressions("select")->setDelimiter(", ")->getSql($context);
 		} else {
 			$sql .= " * ";
 		}
 
-		if (count($this->getExpressions('from'))) {
-			$sql .= " FROM " . $this->getExpressions('from')->setDelimiter(", ")->getSql($context);
+		if (count($this->getExpressions("from"))) {
+			$sql .= " FROM " . $this->getExpressions("from")->setDelimiter(", ")->getSql($context);
 		}
 
-		if (count($this->getExpressions('join'))) {
-			$sql .= $this->getExpressions('join')->setDelimiter(" ")->getSql($context);
+		if (count($this->getExpressions("join"))) {
+			$sql .= $this->getExpressions("join")->setDelimiter(" ")->getSql($context);
 		}
 
-		if (count($this->getExpressions('where'))) {
-			$sql .= " WHERE " . $this->getExpressions('where')->setDelimiter(" AND ")->getSql($context);
+		if (count($this->getExpressions("where"))) {
+			$sql .= " WHERE " . $this->getExpressions("where")->setDelimiter(" AND ")->getSql($context);
 		}
 
-		if (count($this->getExpressions('groupBy'))) {
-			$sql .= " GROUP BY " . $this->getExpressions('groupBy')->setDelimiter(", ")->getSql($context);
+		if (count($this->getExpressions("groupBy"))) {
+			$sql .= " GROUP BY " . $this->getExpressions("groupBy")->setDelimiter(", ")->getSql($context);
 		}
 
-		if (count($this->getExpressions('having'))) {
-			$sql .= " HAVING " . $this->getExpressions('having')->setDelimiter(" AND ")->getSql($context);
+		if (count($this->getExpressions("having"))) {
+			$sql .= " HAVING " . $this->getExpressions("having")->setDelimiter(" AND ")->getSql($context);
 		}
 
-		if (count($this->getExpressions('orderBy'))) {
-			$sql .= " ORDER BY " . $this->getExpressions('orderBy')->setDelimiter(", ")->getSql($context);
+		if (count($this->getExpressions("orderBy"))) {
+			$sql .= " ORDER BY " . $this->getExpressions("orderBy")->setDelimiter(", ")->getSql($context);
 		}
 
 		if ($this->getLimit()) {
@@ -95,20 +95,20 @@ class Select extends Command
 			$sql .= " LIMIT " . $this->getPage()->getSql($context);
 		}
 
-		if ($this->getFlag('intoOutfile')) {
-			$sql .= " INTO OUTFILE '" . $this->getFlag('intoOutfile') . "' ";
+		if ($this->getFlag("intoOutfile")) {
+			$sql .= " INTO OUTFILE '" . $this->getFlag("intoOutfile") . "' ";
 
-			if ($this->getFlag('fieldsTerminatedBy')) {
-				$sql .= " FIELDS TERMINATED BY '" . $this->getFlag('fieldsTerminatedBy') . "' ";
+			if ($this->getFlag("fieldsTerminatedBy")) {
+				$sql .= " FIELDS TERMINATED BY '" . $this->getFlag("fieldsTerminatedBy") . "' ";
 			}
-			if ($this->getFlag('optionallyEnclosedBy')) {
-				$sql .= " OPTIONALLY ENCLOSED BY '" . $this->getFlag('optionallyEnclosedBy') . "' ";
+			if ($this->getFlag("optionallyEnclosedBy")) {
+				$sql .= " OPTIONALLY ENCLOSED BY '" . $this->getFlag("optionallyEnclosedBy") . "' ";
 			}
-			if ($this->getFlag('escapedBy')) {
-				$sql .= " ESCAPED BY '" . $this->getFlag('escapedBy') . "' ";
+			if ($this->getFlag("escapedBy")) {
+				$sql .= " ESCAPED BY '" . $this->getFlag("escapedBy") . "' ";
 			}
-			if ($this->getFlag('linesTerminatedBy')) {
-				$sql .= " LINES TERMINATED BY '" . $this->getFlag('linesTerminatedBy') . "' ";
+			if ($this->getFlag("linesTerminatedBy")) {
+				$sql .= " LINES TERMINATED BY '" . $this->getFlag("linesTerminatedBy") . "' ";
 			}
 		}
 
