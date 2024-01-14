5e074b68a15d1ab141a672618af0713fe6f3a0e9
Jan Jablunka <jablunka@gmail.com>
2024-01-14 15:42:30 +0100

    incremental

--- src/Limit.php
+++ src/Limit.php
@@ -7,14 +7,38 @@ class Limit extends Expression
 	public $limit;
 	public $offset;
 
-	public function __construct(int $limit = null, int $offset = null)
+	public function __construct(?int $limit = null, ?int $offset = null)
+	{
+		$this->setLimit($limit);
+		$this->setOffset($offset);
+	}
+
+	public function setLimit(?int $limit): Limit
 	{
 		$this->limit = $limit;
+
+		return $this;
+	}
+
+	public function getLimit(): ?int
+	{
+		return $this->limit;
+	}
+
+	public function setOffset(?int $offset): Limit
+	{
 		$this->offset = $offset;
+
+		return $this;
+	}
+
+	public function getOffset(): ?int
+	{
+		return $this->offset;
 	}
 
 	public function getSql(&$context = [])
 	{
-		return " LIMIT " . (!is_null($this->limit) ? $this->limit : "18446744073709551615") . (!is_null($this->offset) ? " OFFSET " . $this->offset : null);
+		return " LIMIT " . (!is_null($this->getLimit()) ? $this->getLimit() : "18446744073709551615") . (!is_null($this->getOffset()) ? " OFFSET " . $this->getOffset() : null);
 	}
 }
